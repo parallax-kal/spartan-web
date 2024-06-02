85246c9171d6e86b2ab4be30ee034fc2f8cb4ebc
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2024-06-02 04:45:28 +0200

    [update]

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -80,7 +80,7 @@ const Navbar: FC<NavbarProps> = () => {
               className={cn(
                 'after:ease-after:ease-&lsqb;cubic-bezier(0.65_0.05_0.36_1)&rsqb; relative font-semibold after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-neutral-800 after:transition-transform after:duration-300 hover:after:origin-bottom-left hover:after:scale-x-100',
                 { 'after:scale-x-50': active == name },
-                // { 'lg:text-white lg:after:bg-white': active == 'Home' && ss <= 300 }
+                { 'lg:text-white lg:after:bg-white': active == 'Home' && ss <= 300 }
               )}
             >
               {name}
