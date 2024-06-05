0350c53f9f5105fd12bcfa532f1a6431bb8320ca
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2024-06-05 10:59:55 +0200

    [fx]

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react';
-import Logo from './Logo';
 import { footerItems } from '@/lib/constants';
 import Link from 'next/link';
 import { Mail, Phone } from 'lucide-react';
@@ -8,7 +7,6 @@ import { Icons } from './Icons';
 const Footer: FC = () => {
   return (
     <div className="mt-14 flex w-full flex-col items-center px-4 sm:px-10">
-      <Logo />
       <div className="mt-10 flex">
         <div className="grid w-full grid-cols-2 gap-5 md:grid-cols-4 md:gap-10 lg:grid-cols-[1.5fr,repeat(4,minmax(0,1fr))] xl:grid-cols-[2.5fr,repeat(4,minmax(0,1fr))]">
           <div className="col-span-2 space-y-3 place-self-center md:col-span-4 lg:col-span-1 lg:place-self-auto">
@@ -38,13 +36,13 @@ const Footer: FC = () => {
           </div>
           {footerItems.map((e, i) => (
             <div key={i} className="flex w-fit flex-col">
-              <h2 className="mb-4 text-base font-bold leading-tight xs:text-lg">{e.title}</h2>
+              <h2 className="xs:text-lg mb-4 text-base font-bold leading-tight">{e.title}</h2>
               {e.items.map((e, i) =>
                 e.as == 'link' ? (
                   <Link
                     key={i}
                     href={e.link}
-                    className="my-1 text-xs leading-tight text-[#5D5C5C] xs:text-sm"
+                    className="xs:text-sm my-1 text-xs leading-tight text-[#5D5C5C]"
                   ></Link>
                 ) : (
                   <p key={i} className="my-1 text-sm text-[#5D5C5C]">
